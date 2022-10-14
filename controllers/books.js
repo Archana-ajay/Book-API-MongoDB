461d81b52244198e1d353548b4fc414c26371d26
archana <archanaajay45@gmail.com>
2022-10-14 19:34:27 +0530

    only show the books created by user

--- controllers/books.js
+++ controllers/books.js
@@ -11,7 +11,7 @@ const getAllBooks=async(req,res)=>{
     res.status(StatusCodes.OK).json({ book, count: book.length })
 }
 
-//create book and save in the databse
+//create book and save it in the databse
 const createBook=async(req,res)=>{
     if (!req.files) {
         throw new BadRequestError('No File Uploaded');
