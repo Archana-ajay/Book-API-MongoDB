284d2c8e44899ef93a2bad5beb15ab3552e12c62
archana <archanaajay45@gmail.com>
2022-10-21 19:40:35 +0530

    implement validation of query params

--- validators/query-params-validator.js
+++ validators/query-params-validator.js
@@ -1,8 +1,8 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const querySchema = Joi.object({
-    limit:Joi.number().integer().min(1).max(100).default(5),
-    page:Joi.number().integer().min(1).default(1)
+    limit: Joi.number().integer().min(1).max(100).default(5),
+    page: Joi.number().integer().min(1).default(1),
 });
 
 module.exports = querySchema;
