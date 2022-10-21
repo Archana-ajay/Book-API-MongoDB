fbe68bac636edf91fce8cbc6efb8ec1a99cd2586
archana <archanaajay45@gmail.com>
2022-10-21 19:28:56 +0530

    signup and login added

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,11 @@
-const CustomAPIError = require('./custom-api')
-const NotFoundError = require('./not-found')
-const BadRequestError=require('./bad-request')
-const UnauthenticatedError=require('./Unauthenticated')
+const CustomAPIError = require('./custom-api');
+const NotFoundError = require('./not-found');
+const BadRequestError = require('./bad-request');
+const UnauthenticatedError = require('./Unauthenticated');
 
 module.exports = {
-  CustomAPIError,
-  NotFoundError,
-  BadRequestError,
-  UnauthenticatedError
-}
+    CustomAPIError,
+    NotFoundError,
+    BadRequestError,
+    UnauthenticatedError,
+};
