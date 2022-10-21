a709b2d0b0064a5e290fe8b2019c05e2dd50fb9c
archana <archanaajay45@gmail.com>
2022-10-21 19:35:15 +0530

    user details added

--- model/Book.js
+++ model/Book.js
@@ -1,34 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const BookSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please provide the name of book'],
-      maxlength: 30
+    {
+        name: {
+            type: String,
+            required: [true, 'Please provide the name of book'],
+            maxlength: 30,
+        },
+        author: {
+            type: String,
+            required: [true, 'Please provide the author of book'],
+            maxlength: 50,
+        },
+        imageUrl: {
+            type: String,
+            required: [true, 'Please provide url of book image'],
+        },
+        price: Number,
+        pages: {
+            type: Number,
+            min: 100,
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide user'],
+        },
     },
-    author: {
-      type: String,
-      required: [true, 'Please provide the author of book'],
-      maxlength: 50
-    },
-    imageUrl: {
-      type: String,
-      required:[true,'Please provide url of book image']
-    
-    },
-    price:Number,
-    pages:{
-        type:Number,
-        min:100
-    },
-    createdBy: {
-      type: mongoose.Types.ObjectId,
-      ref: 'User',
-      required: [true, 'Please provide user'],
-  },
-},
-  { timestamps: true }
-)
+    { timestamps: true }
+);
 
-module.exports = mongoose.model('Book', BookSchema)
+module.exports = mongoose.model('Book', BookSchema);
