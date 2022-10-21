027a50d97042bfcd7e658712d73189b909f76136
archana <archanaajay45@gmail.com>
2022-10-21 19:37:43 +0530

    implement pagination

--- utils/api-features.js
+++ utils/api-features.js
@@ -1,16 +1,16 @@
 class APIFeatures {
     constructor(query, queryString) {
-      this.query = query;
-      this.queryString = queryString;
+        this.query = query;
+        this.queryString = queryString;
     }
-  
+
     paginate() {
-      const page = this.queryString.page * 1 || 1;
-      const limit = this.queryString.limit * 1 || 5;
-      const skip = (page - 1) * limit;
-      this.query = this.query.skip(skip).limit(limit);
-      return this;
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 5;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
     }
-  }
-  
-  module.exports = APIFeatures;
+}
+
+module.exports = APIFeatures;
