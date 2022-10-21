9c6ae0ae4ef7cc1f9ac396eb36399e70335f001c
archana <archanaajay45@gmail.com>
2022-10-21 19:41:28 +0530

    eslint and prettier added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,42 @@
-const User = require('../model/User')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, UnauthenticatedError } = require('../errors')
+const User = require('../model/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 //signup
 const signUp = async (req, res) => {
-  const user = await User.create({ ...req.body })
-  res.status(StatusCodes.CREATED).json({ user:{name:user.name},message:"signup successful"})
-}
+    const user = await User.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({
+        user: { name: user.name },
+        message: 'signup successful',
+    });
+};
 
 //login
 const login = async (req, res) => {
-  const { email, password } = req.body
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    throw new BadRequestError('Please provide email and password')
-  }
-  const user = await User.findOne({ email })
-  if (!user) {                                                      //check email 
-    throw new UnauthenticatedError('Invalid Credentials')
-  }
-  const isPasswordCorrect = await user.comparePassword(password)
-  if (!isPasswordCorrect) {                                         //compare password
-    throw new UnauthenticatedError('Invalid password')
-  }
-  const token = user.createJWT()
-  res.status(StatusCodes.OK).json({ user: { name: user.name },message:"login successful",token})
-}
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        //check email
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+        //compare password
+        throw new UnauthenticatedError('Invalid password');
+    }
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({
+        user: { name: user.name },
+        message: 'login successful',
+        token,
+    });
+};
 
 module.exports = {
-  signUp,
-  login,
-}
+    signUp,
+    login,
+};
--- controllers/books.js
+++ controllers/books.js
@@ -1,82 +1,99 @@
-const Book=require("../model/Book")
+const Book = require('../model/Book');
 const path = require('path');
-const { StatusCodes } = require('http-status-codes')
-const { NotFoundError,BadRequestError } = require('../errors')
-const APIFeatures=require("../utils/api-features")
+const { StatusCodes } = require('http-status-codes');
+const { NotFoundError, BadRequestError } = require('../errors');
+const APIFeatures = require('../utils/api-features');
 
 //retrieve the books created by user from database
-const getAllBooks=async(req,res)=>{
-    const features= new APIFeatures(Book.find({createdBy: req.user.userId }),req.query).paginate()  
-    const book=await features.query
-    res.status(StatusCodes.OK).json({ book, count: book.length })
-}
+const getAllBooks = async (req, res) => {
+    const features = new APIFeatures(
+        Book.find({ createdBy: req.user.userId }),
+        req.query
+    ).paginate();
+    const book = await features.query;
+    res.status(StatusCodes.OK).json({ book, count: book.length });
+};
 
 //retrieve all books not created by the user
-const getRecommendedBooks=async(req,res)=>{
-  const features= new APIFeatures(Book.find({createdBy: {$ne:req.user.userId}}),req.query).paginate()  
-  const book=await features.query
-  res.status(StatusCodes.OK).json({ book, count: book.length })
-}
+const getRecommendedBooks = async (req, res) => {
+    const features = new APIFeatures(
+        Book.find({ createdBy: { $ne: req.user.userId } }),
+        req.query
+    ).paginate();
+    const book = await features.query;
+    res.status(StatusCodes.OK).json({ book, count: book.length });
+};
 
 //create book and save it in the databse
-const createBook=async(req,res)=>{
+const createBook = async (req, res) => {
     if (!req.files) {
         throw new BadRequestError('No File Uploaded');
-      }
-      const bookImage = req.files.imageUrl;
-      const extensionName = path.extname(bookImage.name);
-      const allowedExtension = ['.png','.jpg','.jpeg'];
-      if(!allowedExtension.includes(extensionName)){
+    }
+    const bookImage = req.files.imageUrl;
+    const extensionName = path.extname(bookImage.name);
+    const allowedExtension = ['.png', '.jpg', '.jpeg'];
+    if (!allowedExtension.includes(extensionName)) {
         throw new BadRequestError('Please Upload a valid Image');
     }
-    const imagePath = path.join(
-        __dirname,
-        '../uploads/' + `${bookImage.name}`
-      );
-      await bookImage.mv(imagePath);
-      req.body.imageUrl=`/uploads/${bookImage.name}`
-      req.body.createdBy = req.user.userId
-      const book=await Book.create(req.body)
-      res.status(StatusCodes.CREATED).json({ book })
-}
+    const imagePath = path.join(__dirname, '../uploads/' + `${bookImage.name}`);
+    await bookImage.mv(imagePath);
+    req.body.imageUrl = `/uploads/${bookImage.name}`;
+    req.body.createdBy = req.user.userId;
+    const book = await Book.create(req.body);
+    res.status(StatusCodes.CREATED).json({ book });
+};
 
 //retrieve a single book from database
-const getBook=async(req,res)=>{
+const getBook = async (req, res) => {
     const {
         user: { userId },
         params: { id: bookID },
-      } = req
-        const book=await Book.findOne({_id:bookID,createdBy:userId})
-        if(!book){
-            throw new NotFoundError(`No book with id ${bookID}`)
-        }
-        res.status(StatusCodes.OK).json({ book })
-}
+    } = req;
+    const book = await Book.findOne({ _id: bookID, createdBy: userId });
+    if (!book) {
+        throw new NotFoundError(`No book with id ${bookID}`);
+    }
+    res.status(StatusCodes.OK).json({ book });
+};
 
 //update a book in database
-const updateBook=async(req,res)=>{
+const updateBook = async (req, res) => {
     const {
         user: { userId },
         params: { id: bookID },
-      } = req
-        const book=await Book.findOneAndUpdate({_id:bookID,createdBy: userId},req.body,{new:true,runValidators:true})
-        if(!book){
-            throw new NotFoundError(`No book with id ${bookID}`)
-        }
-        res.status(StatusCodes.OK).json({ book })
-}
+    } = req;
+    const book = await Book.findOneAndUpdate(
+        { _id: bookID, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    if (!book) {
+        throw new NotFoundError(`No book with id ${bookID}`);
+    }
+    res.status(StatusCodes.OK).json({ book });
+};
 
 //delete a book from database
-const deleteBook=async(req,res)=>{
+const deleteBook = async (req, res) => {
     const {
         user: { userId },
         params: { id: bookID },
-      } = req
-        const book=await Book.findOneAndDelete({_id:bookID,createdBy: userId})
-        if(!book){
-            throw new NotFoundError(`No book with id ${bookID}`)
-        }
-        res.status(StatusCodes.OK).json({ book })
-}
+    } = req;
+    const book = await Book.findOneAndDelete({
+        _id: bookID,
+        createdBy: userId,
+    });
+    if (!book) {
+        throw new NotFoundError(`No book with id ${bookID}`);
+    }
+    res.status(StatusCodes.OK).json({ book });
+};
 
-module.exports={getAllBooks,getRecommendedBooks,createBook,getBook,updateBook,deleteBook}
+module.exports = {
+    getAllBooks,
+    getRecommendedBooks,
+    createBook,
+    getBook,
+    updateBook,
+    deleteBook,
+};
