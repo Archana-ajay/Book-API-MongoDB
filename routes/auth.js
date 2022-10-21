70775986500e284db1d9107d934ccbe8f4fe0741
archana <archanaajay45@gmail.com>
2022-10-21 19:33:35 +0530

    implement validation of query params

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const {bodyMiddleware} = require('../middleware/validator')
-const { signUp, login } = require('../controllers/auth')
+const express = require('express');
+const router = express.Router();
+const { bodyMiddleware } = require('../middleware/validator');
+const { signUp, login } = require('../controllers/auth');
 
-router.post('/signup',bodyMiddleware('register'),signUp)
-router.post('/login',bodyMiddleware('login'),login)
+router.post('/signup', bodyMiddleware('register'), signUp);
+router.post('/login', bodyMiddleware('login'), login);
 
-module.exports = router
+module.exports = router;
